a2d16cef12c3aac009085fca82a0f190ee41de1d
Salatiel Aizza <salatiel.aizza@gmail.com>
2021-01-15 01:19:12 +0000

    FrontEnd: criação inicial da página do dashboard e no BackEnd: Adição de um novo Spot

--- frontend/src/Pages/Dashboard/index.js
+++ frontend/src/Pages/Dashboard/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 export default function Dashboard(){
+    const [spots, setSpots] = useState([]);
+
+useEffect(() => {
+    async function loadSpots(){
+        const user_id = localStorage.getItem('user');
+        const response = await api.get('/dashboard', {
+            headers: { user_id }
+        })
+
+        setSpots(response.data);
+    }
+
+    loadSpots();
+ }, []) //o array vazio indica que será executado uma única vez para o componente
+
     return (
-        <h1>Dashboard</h1>
+        <>
+            <ul className='spot-list'>
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+
+
+        </>
     )
 }
